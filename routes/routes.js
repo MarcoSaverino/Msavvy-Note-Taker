98706d14b47312070ac08f36f4ba09564cd06ad4
MarcoSaverino <marcosaverino@live.ca>
2023-06-01 13:48:55 -0400

    added ReadME and screenshots

--- routes/routes.js
+++ routes/routes.js
@@ -11,28 +11,26 @@ module.exports = app => {
 
         // API ROUTES
     
-        // Setup  /api/notes get route
+
         app.get("/api/notes", function(req, res) {
             res.json(notes);
         });
 
-        // Setup /api/notes post route
         app.post("/api/notes", function(req, res) {
             let newNote = req.body;
             notes.push(newNote);
-            updateDb();
+            update_Db();
             return console.log("Added new note: "+newNote.title);
         });
 
-        // Retrieve note with certain id
         app.get("/api/notes/:id", function(req,res) {
             res.json(notes[req.params.id]);
         });
 
-        // Deletes a note with certain id
+
         app.delete("/api/notes/:id", function(req, res) {
             notes.splice(req.params.id, 1);
-            updateDb();
+            update_Db();
             console.log("Deleted note with id "+req.params.id);
         });
 
@@ -47,7 +45,7 @@ module.exports = app => {
         });
 
         //updates json file
-        function updateDb() {
+        function update_Db() {
             fs.writeFile("db/db.json",JSON.stringify(notes,'\t'),err => {
                 if (err) throw err;
                 return true;
